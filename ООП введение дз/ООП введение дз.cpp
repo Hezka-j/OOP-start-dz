b5f0b5169819aab360d2e6904d524bdeaf6a1c66
Hezka-j <145163687+Hezka-j@users.noreply.github.com>
2023-11-30 23:41:46 +0200

    Add files via upload

---
+++ ООП введение дз/ООП введение дз.cpp
@@ -0,0 +1,193 @@
+﻿#include <iostream>
+using namespace std;
+class Dog {
+public:
+    bool tail;
+    int count_ears;
+    int count_legs;
+    bool wool;
+    string name;
+    string breed;
+    string size;
+
+    void SayGav()
+    {
+        cout << "Gav gav\n";
+    }
+
+    void SetSleep()
+    {
+        cout << "quiet time\n";
+    }
+
+    void StudyComands()
+    {
+        cout << "study\n";
+    }
+
+    void SetLick()
+    {
+        cout << "dog lick you\n";
+    }
+
+    void SetEat()
+    {
+        cout << "dog is eating\n";
+    }
+
+    void SetDrink()
+    {
+        cout << "dog is drinking water\n";
+    }
+
+    void Walking()
+    {
+        cout << "Gav gav go to outside\n";
+    }
+};
+
+class Computer {
+public:
+
+    int version;
+    double price;
+    string model;
+    string desktop;
+    string keyboard;
+    string color;
+
+    void Calculate()
+    {
+        cout << "Calculator\n";
+    }
+
+    void SaveFiles()
+    {
+        cout << "Files saves\n";
+    }
+
+    void MakeNoise()
+    {
+        cout << "sssshhhhhhh\n";
+    }
+
+    void BreakDown()
+    {
+        cout << "ops\n";
+    }
+
+    void SwitchIllumination()
+    {
+        cout << "Illumination on keyboard\n";
+    }
+};
+
+class Juice {
+    int line;
+    double price;
+    string title;
+    string taste;
+    string ingredients;
+    bool fresh_made;
+
+    void Overstay()
+    {
+        cout << "this bad idea\n";
+    }
+
+    void BeTasty()
+    {
+        cout << "delicious\n";
+    }
+
+    void Evaporate()
+    {
+        cout << "juice is gone\n";
+    }
+
+    void MakeBubbles()
+    {
+        cout << "juice is dead\n";
+    }
+
+    void BitSpilled()
+    {
+        cout << "Bad news(\n";
+    }
+};
+
+class Lamp {
+public:
+    int model;
+    int line;
+    int count_lights;
+    double price;
+    string color;
+    string brand;
+
+    void TurnOn()
+    {
+        cout << "light\n";
+    }
+
+    void TurnOf()
+    {
+        cout << "dark\n";
+    }
+
+    void SelectLightLevel()
+    {
+        cout << "dark light more brightly\n";
+    }
+
+    void BurnOut()
+    {
+        cout << "(\n";
+    }
+
+    void BlowUp()
+    {
+        cout << "ops, bad lamp\n";
+    }
+};
+
+class Kino {
+    double score;
+    int count_coments;
+    string appellation;
+    string genre;
+    string topic;
+    string director;
+
+    void MovieEntrance()
+    {
+        cout << "date when we can see movie\n";
+    }
+
+    void RackBrain()
+    {
+        cout << "difficult to brain\n";
+    }
+
+    void TellStory()
+    {
+        cout << "Intresting film\n";
+    }
+
+    void EducatePeople()
+    {
+        cout << "Films about history\n";
+    }
+
+    void UpliftMood()
+    {
+        cout << "Happy films\n";
+    }
+};
+
+int main()
+{
+    
+    
+        
+}
